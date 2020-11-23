59176edceb55bfb1f9357f68f131d56dac97d1e9
Kenta Nakamura <c60evaporator@gmail.com>
2020-11-23 23:22:09 +0900

    メール送信＆検知ログ作成処理追加

--- pi_atlas_utility.py
+++ pi_atlas_utility.py
@@ -7,6 +7,9 @@ import logging
 from pit import Pit
 import ast
 import os
+import csv
+from email import message
+import smtplib
 
 #処理名一覧
 BACKUP_PROCESS_NAME = 'Backup'
@@ -14,6 +17,7 @@ DELETE_PROCESS_NAME = 'Delete'
 BATTERY_PROCESS_NAME = 'Battery'
 ACQUISITION_PROCESS_NAME = 'Acquisition'
 PROCESS_NAME_LIST = [BACKUP_PROCESS_NAME, DELETE_PROCESS_NAME, BATTERY_PROCESS_NAME, ACQUISITION_PROCESS_NAME]
+MAIL_PROCESS_NAMES = [BATTERY_PROCESS_NAME, ACQUISITION_PROCESS_NAME]#検知ログ作成＆メール送信対象処理
 
 class PiAtlasUtility():
     #初期化
@@ -25,6 +29,14 @@ class PiAtlasUtility():
         self.battery_collection_name = None  # 電池切れ予兆検知対象のコレクション名
         self.battery_detection_list = None  # 電池切れ予兆検知対象のセンサリスト
         self.acquisition_detection_list = None  # センサ未取得検知対象のセンサリスト
+        self.detection_output = None  # 検知ログの出力先
+        self.mail_period = None  # メール送信最短間隔
+        self.smtp_host = None  #メールホスト
+        self.smtp_port = None  # メールポート
+        self.from_email = None  # 送信元メールアドレス
+        self.to_email = None  # 送信先メールアドレス
+        self.username = None  # メールユーザ名
+        self.password = None  # メールパスワード
 
     #電池切れ予兆検知
     def _battery_anomaly_detection(self, atlasclient, collection_name):
@@ -53,11 +65,20 @@ class PiAtlasUtility():
 
             #上下限オーバーしたデータ数がCount以上のとき、電池切れ予兆発生判定
             if len(df_anomaly) >= device.Count:
-                message = f'BATTERY ANOMALY DETECTION [device {device.DeviceName}, date {str(self.masterdate)}]'
-                logging.critical(message)
-                #メール送信
-                message = message + '\n' + str(df_anomaly.drop(['_id'], axis=1).reset_index(drop=True))
-                self._send_email(message)
+                #メールのタイトル
+                mail_title = f'BATTERY ANOMALY [{device.DeviceName}, {self.masterdate.strftime("%Y/%m/%d %H:%M:%S")}]'
+                logging.info(mail_title)
+                #メールの本文
+                mail_message = f'BATTERY ANOMALY DETECTION\n'\
+                    f'device {device.DeviceName}\n'\
+                    f'date {str(self.masterdate)}]'
+                mail_message = mail_message + '\n\n' + str(df_anomaly.drop(['_id'], axis=1).reset_index(drop=True))
+                #一定時間内にメール送信されたか確認し、Falseならメール送信＋検知ログ書込、Trueなら検知ログ書込のみ実施
+                if self._confirm_mail_sent(device.DeviceName):
+                    self._make_detection_log(device.DeviceName, False)
+                else:
+                    self._make_detection_log(device.DeviceName, True)
+                    self._send_email(mail_title, mail_message)
 
     #センサ未取得検知
     def _acquisition_anomaly_detection(self, atlasclient, collection_name):
@@ -74,7 +95,7 @@ class PiAtlasUtility():
             colname = f"no{format(device.No,'02d')}_{device.ColName}"
             #FailureMinutes以降のデータのみ抽出
             device_start = self.masterdate - timedelta(minutes=device.FailureMinutes)
-            df_device = df[df['Date_Master'] >= device_start]
+            df_device = df[df['Date_Master'] >= device_start][["Date_Master",colname]]
             # 判定対象列が存在するとき、判定対象列の取得成功数をカウント
             if colname in df_device.columns:
                 acquisition_num = df_device[colname].count()
@@ -83,16 +104,73 @@ class PiAtlasUtility():
                 acquisition_num = 0
             # 取得成功数=0のとき、
             if acquisition_num == 0:
-                message = f'ACQUISITION FAILURE DETECTION [device {device.DeviceName}, date {str(self.masterdate)}]'
-                self._send_email(message)
+                #メールのタイトル
+                mail_title = f'ACQUISITION FAILURE [{device.DeviceName}, {self.masterdate.strftime("%Y/%m/%d %H:%M:%S")}]'
+                logging.info(mail_title)
+                #メールの本文
+                mail_message = f'ACQUISITION FAILURE DETECTION\n'\
+                    f'device {device.DeviceName}\n'\
+                    f'date {str(self.masterdate)}]'
+                mail_message = mail_message + '\n\n' + str(df_device.reset_index(drop=True))
+                #一定時間内にメール送信されたか確認し、Falseならメール送信＋検知ログ書込、Trueなら検知ログ書込のみ実施
+                if self._confirm_mail_sent(device.DeviceName):
+                    self._make_detection_log(device.DeviceName, False)
+                else:
+                    self._make_detection_log(device.DeviceName, True)
+                    self._send_email(mail_title, mail_message)
             
     # 検知ログの作成
-    def _make_detection_log(self):
-        print('')
+    def _make_detection_log(self, device_name, send_email):
+        #出力するデータ
+        output_dict = {'Date_Master':str(self.masterdate.strftime("%Y/%m/%d %H:%M:%S")),
+        'Device_Name': device_name,
+        'Send_Email': send_email}
+        #検知ログのパス
+        outpath = f'{self.detection_output}/{self.process_name}DetectionLog_{self.masterdate.year}.csv'
+        #検知ログ存在しないとき、新たに作成
+        if not os.path.exists(outpath):        
+            with open(outpath, 'w', newline="") as f:
+                writer = csv.DictWriter(f, output_dict.keys())
+                writer.writeheader()
+                writer.writerow(output_dict)
+        #検知ログ存在するとき、1行追加
+        else:
+            with open(outpath, 'a', newline="") as f:
+                writer = csv.DictWriter(f, output_dict.keys())
+                writer.writerow(output_dict)
+    
+    # 規定時間以内にメールが送られたか確認
+    def _confirm_mail_sent(self, device_name):
+        #検知ログのパス
+        detection_path = f'{self.detection_output}/{self.process_name}DetectionLog_{self.masterdate.year}.csv'
+        #検知ログ存在しないとき、false
+        if not os.path.exists(detection_path):
+            return False
+        #検知ログ存在するとき、規定時間以内にメールが送られたか確認
+        else:
+            df_detection = pd.read_csv(detection_path, parse_dates=['Date_Master'])
+            last_mail_date = df_detection[df_detection['Device_Name'] == device_name]['Date_Master'].max()
+            if last_mail_date > self.masterdate - timedelta(hours=self.mail_period):
+                return True
+            else:
+                return False
 
-    #メール送信
-    def _send_email(self, message):
-        print(message)
+
+    #メール送信(https://qiita.com/aj2727/items/81e5d67cbcbf7396e392)
+    def _send_email(self, mail_title, mail_message):
+        msg = message.EmailMessage()
+        msg.set_content(mail_message)
+        msg['Subject'] = mail_title
+        msg['From'] = self.from_email
+        msg['To'] = self.to_email
+
+        server = smtplib.SMTP(self.smtp_host, self.smtp_port)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(self.username, self.password)
+        server.send_message(msg)
+        server.quit()
 
     #処理本体を実行
     def _run_process(self, user_name, cluster_name, db_name, collection_name, retry):
@@ -163,6 +241,19 @@ class PiAtlasUtility():
         #ログの初期化
         logname = f"/Atlas{self.process_name}Log_{str(self.masterdate.strftime('%y%m%d'))}.log"
         logging.basicConfig(filename=log_output + logname, level=logging.INFO)
+
+        #メール送信＆検知ログ出力対象処理のとき
+        if self.process_name in MAIL_PROCESS_NAMES:
+            self.mail_period = int(cfg['Date'][f'{self.process_name}MailPeriod'])#メール送信最短間隔
+            self.smtp_host = cfg['Mail']['SmtpHost']  #メールホスト
+            self.smtp_port = int(cfg['Mail']['SmtpPort'])  # メールポート
+            self.from_email = cfg['Mail']['FromEmail']  # 送信元メールアドレス
+            self.to_email = cfg['Mail']['ToEmail']  # 送信先メールアドレス
+            self.username = cfg['Mail']['UserName']  # メールユーザ名
+            self.password = cfg['Mail']['Password']  # メールパスワード
+            self.detection_output = cfg['Path'][f'{self.process_name}DetectionOutput']#検知ログ出力先
+            if not os.path.exists(self.detection_output): # 出力ディレクトリ存在しなければ作成
+                os.makedirs(self.detection_output)
 
         #コレクション(テーブル)を走査
         for k in collection_names.keys():
